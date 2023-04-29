a55a2266a9b823482faf47cdc968a06201566007
Ivan Gudak <ivan.gudak@dynatrace.com>
2023-04-29 11:54:23 +0200

    fix names in the push scripts

--- push_docker.sh
+++ push_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker image build --platform linux/amd64 -t ivangudak096/dt-java-agents-x64:latest .
-docker push ivangudak096/dt-java-agents-x64:latest
+docker image build --platform linux/amd64 -t ivangudak096/java-agents-x64:latest .
+docker push ivangudak096/java-agents-x64:latest
 
-docker image build --platform linux/arm64/v8 -t ivangudak096/dt-java-agents-arm64:latest .
-docker push ivangudak096/dt-java-agents-arm64:latest
+docker image build --platform linux/arm64/v8 -t ivangudak096/java-agents-arm64:latest .
+docker push ivangudak096/java-agents-arm64:latest
